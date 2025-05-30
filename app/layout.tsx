4f744c88a4b4dae23d30e4ef7d899c3b9d99ab12
nicerazer <amocuk@gmail.com>
2025-05-31 00:11:03 +0800

    Updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,25 @@
+
+import { Jua } from 'next/font/google'
+
+const jua = Jua({subsets:['latin'], weight:'400'});
+// const jua = Jua({
+//   // subsets: ['latin'],
+// })
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={jua.className}>
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
         <link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet"/>
-        <link href="./output.css" rel="stylesheet"></link>        
+        {/* <link href="./output.css" rel="stylesheet"></link> */}
       </head>
       <body>
         <main>          
